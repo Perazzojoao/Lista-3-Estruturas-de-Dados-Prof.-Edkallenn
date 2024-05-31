30b8345f3f90e90482d68e754371a3c1cd4f6fca
Perazzojoao <perazzojoao@gmail.com>
2024-05-31 01:54:18 -0300

    Add questões

---
+++ ED-lista3-questao06.c
@@ -0,0 +1,45 @@
+/*
+** Função: Função para calcular a raiz quadrada de um número n com aproximação de 0.0001.
+** Autor: João Victor Barbosa Perazzo
+** Data: 25/05/2024
+** Observações:
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+#define EPSILON 0.0001
+
+double raizQuadrada(double n);
+
+int main() {
+  setlocale(LC_ALL, "Portugese");
+
+  double n;
+
+  printf("Digite um número: ");
+  scanf("%lf", &n);
+  fflush(stdin);
+
+  double r = raizQuadrada(n);
+
+  printf("A raiz quadrada de %f é %f\n", n, r);
+
+  return 0;
+}
+
+double raizQuadrada(double n) {
+  double x = 1;
+  double r = 0;
+
+  while (1) {
+    r = (x + n / x) / 2;
+    if (fabs(r - x) < EPSILON) {
+      break;
+    }
+    x = r;
+  }
+
+  return r;
+}
